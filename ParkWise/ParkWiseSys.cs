c7e339afe9c9fe471646be17b7ba88b71b6d7361
Rielly Young <rielly.young11@gmail.com>
2023-03-05 17:25:30 -0500

    rename directories/files

---
+++ ParkWise/ParkWiseSys.cs
@@ -0,0 +1,32 @@
+
+
+// System file calling other cs files in dir.
+// * indicates nice-to-have
+
+
+/* FEATURE: Database of parking lots under management (Booking.cs)
+    - Ability to read/write to database
+    - Tracks time-in/time-out for each vehicle
+    - 
+
+*/
+
+/* FEATURE: Monthly parking pass/subscription tracking
+    - Some lots have monlthy subs
+    - 
+*/
+
+/* FEATURE: Ticketing system (Ticketing.cs)
+    - Linked to database
+    - Shows amount paid; time-in/time-out; total amount of time 
+*/
+
+/* FEATURE: Payment system (Payment.cs)
+    - Payment logic
+    - Payment database
+    - Accounting/reporting functionality*
+*/
+
+/* FEATURE: Online customer payments (Payment.cs)
+    - 
+*/
